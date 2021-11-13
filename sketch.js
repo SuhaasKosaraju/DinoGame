8fdb0473ebfb713d6a573cb5b64d31515e7cbc5a
SuhaasKosaraju <94168845+SuhaasKosaraju@users.noreply.github.com>
2021-11-13 11:09:04 +1100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var marks = [43,12,50,21,79]
+
+
+
+
+
+
+
+function average(){
+ var sum  = marks[0]+marks[1]+marks[2]+marks[3]+marks[4] 
+ var avg = sum/marks.length 
+ console.log(avg)
+
+}
+
+average();
+
+
+
+
+
+
+
+
